4bb4dbb5282b2e629a306c515739d74fd8784417
Beto L <albertosveloso@gmail.com>
2018-10-30 17:15:48 -0300

    Testando a configuracao

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 const webpack = require('webpack')
 
-import react from 'React'
-
 module.exports = {
     entry: './ex/index.js',
     output: {
         path: __dirname + '/public',
         filename: './bundle.js'
     },
-    devserver: {
+    devServer: {
         port: 8080,
         contentBase: './public'
     }
